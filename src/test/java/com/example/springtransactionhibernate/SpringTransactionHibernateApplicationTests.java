347c5fcc63e082ce7a18f250854b89b0c42e594a
Vadym <vadym.snigur.code@gmail.com>
2022-02-08 10:49:52 +0200

    first commit

---
+++ src/test/java/com/example/springtransactionhibernate/SpringTransactionHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtransactionhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
